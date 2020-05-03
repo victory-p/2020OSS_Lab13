ba512ace1e8d3b4a714a5b53c46433bd40760ae3
Park Seungri <21900282@handong.edu>
2020-05-03 18:52:38 +0900

    Update phonenum.c

--- phonenum.c
+++ phonenum.c
@@ -2,7 +2,21 @@
 #include <string.h>
 #include "phonenum.h"
 
-//int createPhonenum(PhoneNum *p){};
+int createPhonenum(PhoneNum *p){
+	printf("이름 : ");
+	scanf("%s", p->name);
+	printf("전화번호 : ");
+	scanf("%s", p->phonenum);
+	printf("관계 : ");
+	scanf("%s", p->relative);
+	printf("생일(MMDD) : ");
+	scanf("%s", p->birth);
+	printf("주소 : ");
+	scanf("%s", p->address);
+	printf(">>추가 완료<<\n");
+
+	return 1;
+}
 //void readPhonenum(PhoneNum p){};
 //int updatePhoneNum(PhoneNum *p){};
 //int deletePhoneNum(PhoneNum *p){};
