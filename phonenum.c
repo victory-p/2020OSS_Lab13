d6c5971cd70212f6b6b340f8856c0ebfbadaf15b
Oh yoonjin <95potter95@google.com>
2020-05-03 10:49:15 +0900

    Create phonenum.c

---
+++ phonenum.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include "phonenum.h"
+
+//int createPhonenum(PhoneNum *p){};
+//void readPhonenum(PhoneNum p){};
+//int updatePhoneNum(PhoneNum *p){};
+//int deletePhoneNum(PhoneNum *p){};
+
+//int selectMenu(){};
+//void listPhoneNum(PhoneNum *p,int count){};
+//int selectDataNo(PhoneNum *p, int count){};
+
+//void saveData(PhoneNum p[], int count){};
+//int loadData(PhoneNum p[]){};
+
+//void searchName(PhoneNum *p, int count){}; //이름 검색 함수
+//void searchPhonenum(PhoneNum *p, int count){};//전화 번호  검색 함수
+//void searchRelative(PhoneNum *p, int count){};//관계  검색 함수
+//void searchBirth(PhoneNum *p, int count){};//생일 검색 함수
+//void searchAddress(PhoneNum *p, int count){};//주소  검색 함수
+	
